6098768ff818881055b713c6c427f626150c00f6
Koby Fink <finkkoby@icloud.com>
2024-03-01 14:36:45 -0500

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,42 @@
 // Your tests here
+import { isPalindrome } from "../utils"
+
+describe('isPalindrome', () => {
+    it("returns true if the word passed in is a palindrome", () => {
+        const word = 'racecar'
+        const bool = isPalindrome(word)
+        expect(bool).toBe(true)
+    })
+
+    it("returns false is the word passed in is NOT a palindrome", () => {
+        const word = 'car'
+        const bool = isPalindrome(word)
+        expect(bool).toBe(false)
+    })
+
+    it("is case insensitive", () => {
+        const word = 'RaceCar'
+        const bool = isPalindrome(word)
+        expect(bool).toBe(true)
+    })
+
+    it("returns false for an empty string", () => {
+        const word = ''
+        const bool = isPalindrome(word)
+        expect(bool).toBe(false)
+    })
+
+    it("throws an error for any input containing non-alphabetic characters", () => {
+        const word = 'tic-tac'
+        expect(() => {
+            isPalindrome(word)
+        }).toThrow()
+    })
+
+    it("throws an error for any non-string input", () => {
+        const word = [1, 2, 3]
+        expect(() => {
+            isPalindrome(word)
+        }).toThrow()
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,11 @@
 // Your code here
+
+export function isPalindrome(word) {
+    for (let char of word) {
+        if (/^[A-Za-z]+$/.test(word) === false || typeof(word) !== 'string') {
+            throw "Invalid input."
+        }
+    }
+    let reversed = word.split('').reverse().join('')
+    return reversed.toLowerCase() === word.toLowerCase() && word.length > 0
+}
